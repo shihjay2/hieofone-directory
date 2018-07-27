3c9de9484b98d1846adcf1c299b8c6e068310eed
Michael Shihjay Chen <shihjay2@gmail.com>
2018-07-27 16:38:05 -0700

    Fix OIDC relay errors

---
+++ database/migrations/2018_07_27_233427_add_error_to_oidc_relay_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddErrorToOidcRelayTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('oidc_relay', function (Blueprint $table) {
+            $table->longtext('error')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('oidc_relay', function (Blueprint $table) {
+            //
+        });
+    }
+}
