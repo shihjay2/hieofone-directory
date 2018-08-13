88193611a588fe797742df758580bb6148bdfddb
Michael Shihjay Chen <shihjay2@gmail.com>
2018-08-12 17:51:48 -0700

    UI fix

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -15,7 +15,7 @@
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-2">
-								<p>For Clinician access to consenting Trusttee health records, you'll need a smartphone with the <a href="https://uport.me" target="_blank">uPort app</a> installed first.
+								<p>For Clinician access to consenting Trustee health records, you'll need a smartphone with the <a href="https://uport.me" target="_blank">uPort app</a> installed first.
 								<a href="https://uport.me" target="_blank"><img class="img-responsive" src="{{ asset('assets/uPortApp.png') }}"></img></a>
 								<br><p>Then add your NPI to your uPort app through the Doximity credential verification site</p>
                                 <button type="button" class="btn btn-primary btn-block" id="connectUportBtn1"><i class="fa fa-btn fa-plus"></i> Add Doximity Clinician Verification</button>
