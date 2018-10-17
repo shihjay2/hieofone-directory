3c36a9b66ee64115e89f1c3a326c8728d53f9f3d
Michael Shihjay Chen <shihjay2@gmail.com>
2018-10-17 15:25:38 -0700

    Update Socialite google command

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -3012,7 +3012,8 @@ class OauthController extends Controller
                 config(['services.google.client_id' => env('GOOGLE_KEY')]);
                 config(['services.google.client_secret' => env('GOOGLE_SECRET')]);
                 config(['services.google.redirect' => env('GOOGLE_REDIRECT_URI')]);
-                $user = Socialite::driver('google')->with(['state' => $state])->user();
+                $user = Socialite::driver('google')->stateless()->user();
+                // $user = Socialite::driver('google')->with(['state' => $state])->user();
                 $token = $user->token;
                 $data2 = [
                     'access_token' => $user->token,
