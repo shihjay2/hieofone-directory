92ac01ac275e4d4f1554b58908da53aad0769f04
Michael Shihjay Chen <shihjay2@gmail.com>
2018-12-05 21:15:45 -0800

    Update proxy process

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -3066,7 +3066,7 @@ class OauthController extends Controller
                             $data3['error'] = 'Credentials associated with the authorization server do not match.  Process cancelled.';
                             $data3['error'] .= '  First: ' . $proxy_verify[$as->type];
                             $data3['error'] .= '.  Second: ' . $oidc_check;
-                            $data3['error'] .= '.  Access token: ' . $json;
+                            $data3['error'] .= '.  Access token: ' . json_encode($json);
                             DB::table('oidc_relay')->where('state', '=', $state)->update($data3);
                             Session::forget('oidc_state');
                             return redirect($as->response_uri);
