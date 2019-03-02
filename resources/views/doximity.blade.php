0b848ccb3cf7e3b24dca0639992e32504c10d657
Michael Shihjay Chen <shihjay2@gmail.com>
2019-03-02 14:16:26 -0800

    Testing

--- resources/views/doximity.blade.php
+++ resources/views/doximity.blade.php
@@ -132,10 +132,10 @@
 	// Setup
 	const Connect = window.uportconnect.Connect;
 	const appName = 'Doximity';
-	const connect = new Connect(appName, [
-		'clientId': '2okWub26m6S7ibjna7j1QEb9tya2LfQieSE',
+	const connect = new Connect(appName, {
+		clientId: '2okWub26m6S7ibjna7j1QEb9tya2LfQieSE',
 		// 'signer': window.uportconnect.SimpleSigner('58e9a23b542693004be35db8233389baf6646e9e88b2110ac737559ae9f8b9f9'),
-		'network': 'rinkeby'
+		network: 'rinkeby'
 	]);
 	const web3 = connect.getWeb3();
 
